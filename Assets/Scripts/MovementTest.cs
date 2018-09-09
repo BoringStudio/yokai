eb6da3f54fdf3ee6d363b1c3d3896beec0826a15
Ivan <reide740@gmail.com>
2018-09-09 19:45:09 +0300

    Made basic ghosts movement

---
+++ Assets/Scripts/MovementTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementTest : MonoBehaviour
+{
+    public GameObject target;
+
+    NavMeshAgent _agent;
+    Animator _animator;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+    
+	void Update ()
+    {
+        if (target.activeInHierarchy)
+        {
+            _agent.SetDestination(target.transform.position);
+        }
+        
+        _animator.SetFloat("Speed", _agent.velocity.magnitude);
+	}
+}
