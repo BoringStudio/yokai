d2205e414827e98f191540e6bd698ca789bb4e80
Ivan <reide740@gmail.com>
2018-09-23 17:05:36 +0300

    Added book input module

---
+++ Assets/Scripts/BookInputModule.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class BookInputModule : StandaloneInputModule
+{
+    public RenderTexture bookRenderTexture;
+    public BookLaserPointer pointer;
+
+    bool _lastPressed;
+
+    Vector2 m_cursorPos;
+    private readonly MouseState m_MouseState = new MouseState();
+    protected override MouseState GetMousePointerEventData(int id = 0)
+    {
+        MouseState m = new MouseState();
+
+        // Populate the left button...
+        PointerEventData leftData;
+        var created = GetPointerData(kMouseLeftId, out leftData, true);
+
+        leftData.Reset();
+
+        if (created)
+            leftData.position = m_cursorPos;
+
+        // Ordinarily we'd just pass the screen coordinates of the cursor through.
+        //Vector2 pos = Input.mousePosition;
+
+        Vector2 pos;
+        pos.x = bookRenderTexture.width * pointer.cursorPosition.x;
+        pos.y = bookRenderTexture.height * pointer.cursorPosition.y;
+        m_cursorPos = pos;
+
+        // For UV-mapped meshes, you could fire a ray against its MeshCollider 
+        // and determine the UV coordinates of the struck point.
+
+        leftData.delta = pos - leftData.position;
+        leftData.position = pos;
+        leftData.scrollDelta = Input.mouseScrollDelta;
+        leftData.button = PointerEventData.InputButton.Left;
+        eventSystem.RaycastAll(leftData, m_RaycastResultCache);
+        var raycast = FindFirstRaycast(m_RaycastResultCache);
+        leftData.pointerCurrentRaycast = raycast;
+        m_RaycastResultCache.Clear();
+
+        // copy the apropriate data into right and middle slots
+        PointerEventData rightData;
+        GetPointerData(kMouseRightId, out rightData, true);
+        CopyFromTo(leftData, rightData);
+        rightData.button = PointerEventData.InputButton.Right;
+
+        PointerEventData middleData;
+        GetPointerData(kMouseMiddleId, out middleData, true);
+        CopyFromTo(leftData, middleData);
+        middleData.button = PointerEventData.InputButton.Middle;
+
+        HandController controller = pointer.GetComponent<HandController>();
+
+        bool pressed = controller.isValid && pointer.GetComponent<HandController>().device.GetPress(SteamVR_Controller.ButtonMask.Trigger);
+
+        PointerEventData.FramePressState pressState;
+        if (pressed && !_lastPressed)
+        {
+            pressState = PointerEventData.FramePressState.Pressed;
+        }
+        else if (!pressed && _lastPressed)
+        {
+            pressState = PointerEventData.FramePressState.Released;
+        }
+        else {
+            pressState = PointerEventData.FramePressState.NotChanged;
+        }
+
+        m_MouseState.SetButtonState(PointerEventData.InputButton.Left, pressState, leftData);
+        m_MouseState.SetButtonState(PointerEventData.InputButton.Right, StateForMouseButton(1), rightData);
+        m_MouseState.SetButtonState(PointerEventData.InputButton.Middle, StateForMouseButton(2), middleData);
+
+        _lastPressed = pressed;
+
+        return m_MouseState;
+    }
+}
---
+++ Assets/Scripts/BookLaserPointer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+[RequireComponent(typeof(LineRenderer))]
+public class BookLaserPointer : MonoBehaviour
+{
+    public Text testLabel;
+
+    public bool cursorInside { get; private set; }
+    public Vector2 cursorPosition { get; private set; }
+
+    LineRenderer _lineRenderer;
+
+	void Start ()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = 2;
+	}
+	
+	void Update ()
+    {
+        Vector3 origin = transform.position;
+
+        _lineRenderer.SetPosition(0, origin);
+
+        RaycastHit hit;
+        if (Physics.Raycast(origin, transform.forward, out hit, Mathf.Infinity, LayerMask.GetMask("UI")))
+        {
+            cursorInside = true;
+            cursorPosition = hit.textureCoord;
+
+            _lineRenderer.enabled = true;
+            _lineRenderer.SetPosition(1, hit.point);
+        }
+        else
+        {
+            cursorInside = false;
+            _lineRenderer.SetPosition(1, origin);
+        }
+
+        testLabel.text = cursorPosition.ToString();
+    }
+}
